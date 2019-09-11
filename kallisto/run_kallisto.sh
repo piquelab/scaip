fcf89900201bcaf9520e1c4539c1eda32fdf67f2
rpique <rpique@gmail.com>
2019-09-10 20:47:18 -0400

    Fixing V3 chem for those samples porcessed as V2

--- kallisto/run_kallisto.sh
+++ kallisto/run_kallisto.sh
@@ -15,7 +15,7 @@ kallistoref=./ref/cDNA_introns.idx
 #find ${fastqfolder} -name 'SCAIP*fastq.gz' | sed 's/.*\///;s/_S.*.fastq.gz//' | sort | uniq > libList.txt
 
 
-cat failed.txt | \
+cat V3list.txt | \
 while read sample; 
 do 
 ##    fastqs=`find ${fastqfolder} -name "${sample}*R[12]*fastq.gz"`
@@ -33,7 +33,7 @@ module unload python;
 module load bustools anaconda3.python;
 ##module load kallisto
 source activate kallisto 
-time kallisto bus -i ${kallistoref} -o ./bus/${sample} -x 10xv2 -t 12 ${fastqlist}" | qsub -q erprq -l nodes=1:ppn=28 -l mem=120g -N $sample
+time kallisto bus -i ${kallistoref} -o ./bus/${sample} -x 10xv3 -t 12 ${fastqlist}" | qsub -q erprq -l nodes=1:ppn=28 -l mem=120g -N $sample
     sleep 0.5;
 done
 
